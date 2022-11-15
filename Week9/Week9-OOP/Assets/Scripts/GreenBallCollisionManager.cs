5d002212ab467d6bb9e2cd8aec6f82c36dd0ce90
MylesC21 <113139790+MylesC21@users.noreply.github.com>
2022-11-15 16:51:28 -0500

    Week9Homework

---
+++ Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+        if (OtherBall.GetComponent<YellowBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Collided With Yellow");
+
+            GetComponent<Renderer>().material.color = new Color(152, 255, 0);
+        }
+    }
+}
+
---
+++ Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        gameObject.transform.localScale += new Vector3(1, 1, 1);
+    }
+}
+
